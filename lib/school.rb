a1f23a7d08c6d101e5b8612a6cc228e3deb3436c
Maurice Fiorani <mauricef97@gmail.com>
2020-02-07 22:42:40 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -10,6 +10,21 @@ class School
   end
   
   def add_student(name, grade)
+   if @roster[grade] != nil
+      @roster[grade] << name
+    else
+      @roster[grade] = [name]
+    end
   end
 
-end
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each do |key, value|
+      value.sort!
+    end
+  end
+end 
+
